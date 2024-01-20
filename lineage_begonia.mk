442a4324f949f295d47b2c6f5effa45affd69abf
decoder-dev <toniron208@gmail.com>
2024-01-20 18:48:57 +0300

    begonia dump with mods...

---
+++ lineage_begonia.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from begonia device
+$(call inherit-product, device/xiaomi/begonia/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_begonia
+PRODUCT_DEVICE := begonia
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 8 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Redmi/begonia/begonia:11/RP1A.200720.011/V12.5.8.0.RGGMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="begonia-user 11 RP1A.200720.011 V12.5.8.0.RGGMIXM release-keys" \
+    PRODUCT_NAME="begonia" \
+    TARGET_DEVICE="begonia"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
